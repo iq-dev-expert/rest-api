585dcd2fa0b9ed418b32ae3809c6873c29cd1e82
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-08-22 23:08:11 +0200

    add mongoose and db host

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Oleksandr:OnkNHKhi2gyV3bUY@cluster0.ptylwch.mongodb.net/db-contacts";
+
+(async () => {
+  try {
+    await mongoose.connect(DB_HOST);
+    console.log("Database connection successful");
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+})();
