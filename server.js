f07878bf576d1a29b3cc3cf04a78706058270349
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-08-23 00:51:10 +0200

    add .env file

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Oleksandr:OnkNHKhi2gyV3bUY@cluster0.ptylwch.mongodb.net/db-contacts";
+const { DB_HOST, PORT } = process.env;
 
 (async () => {
   try {
     await mongoose.connect(DB_HOST);
     console.log("Database connection successful");
 
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   } catch (error) {
