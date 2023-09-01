06d9dc0f9dc7d3ee71475be70e4e55f27ae042fc
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-09-01 18:29:21 +0200

    add filter favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -9,10 +9,16 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt -owner", {
-    skip,
-    limit,
-  });
+  const { favorite = "" } = req.query;
+
+  const result = await Contact.find(
+    { owner, favorite },
+    "-createdAt -updatedAt -owner",
+    {
+      skip,
+      limit,
+    }
+  );
   res.json(result);
 };
 
